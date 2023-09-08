4c0f8a4e1c72c0083026055f5d8c497b238d0c66
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-08 14:45:55 -0600

    Added notes to README.md

--- foobar/README.md
+++ foobar/README.md
@@ -1,1 +1,7 @@
 # JsAlgorithm-learn-env
+
+NOTE: This project uses these dependencies.
+ncy is a code coverage generator which shows how much of our code has been unit tested!
+Mocha is a testing library for node.js
+Chai is an assertion library that is used in conjunction with Mocha.
+    - Assertion libraries help you verify that code is correct.
--- foobar/foobar.js
+++ foobar/foobar.js
@@ -1,14 +1,10 @@
-/*
-The foo bar problem is a coding challenge that is often used as an interview question for software engineers. The problem is to write a function that takes an integer as input and returns the string "Foo" if the integer is divisible by 3, "Bar" if the integer is divisible by 5, or the integer itself if it is not divisible by 3 or 5.
-*/
-
 // Foo Bar refers to US army lingo that = "Fucked up beyond all recognition."
 
 // 1. Takes an integer as an input.
 // 2. if (int) modulus 3 is equal to 0, return strign Foo
 // 3. else if (int) int is modulus 5 is equal to 0 return string Bar 
 // 4. else if both 2. and 3. are truthy, return string FooBar
-// 4. else (int) is not divisible by three or five return the variable storing the integer.
+// 5. else (int) is not divisible by three or five return the variable storing the integer.
 
 let int = 9;
 
